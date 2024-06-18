2b24643c8a13c7a74e765d6827927d74d9e1cc21
NabilMQ <nabilmq13@gmail.com>
2024-06-18 16:49:56 +0700

    Merge branch travelin_amal

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:travelin/controller/color_controller.dart';
 import 'package:travelin/firebase_options.dart';
 import 'package:travelin/view/home/home.dart';
 
