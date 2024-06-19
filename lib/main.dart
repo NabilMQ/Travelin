3eff1ec620be64872286c6885db461ec13768d87
erikosyahputraf <erikosyahputraf@gmail.com>
2024-06-19 03:19:01 +0000

    membuat halaman empty history

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:travelin/firebase_options.dart';
 import 'package:travelin/view/auth/auth.dart';
 import 'package:travelin/view/getStarted/get_started.dart';
+import 'package:travelin/view/history/history.dart';
 import 'package:travelin/view/home/home.dart';
 
 
